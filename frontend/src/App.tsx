122cd30b2efec343d250f73c91504aec22277453
CaptainPMP <62939509+CaptainPMP@users.noreply.github.com>
2023-11-14 09:11:31 +0700

    change it to tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+import Home from './pages/Home';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
