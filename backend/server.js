80d76a5c5c2b6ed7597ea8c9f5ff443b56ceabee
CaptainPMP <62939509+CaptainPMP@users.noreply.github.com>
2023-11-21 11:59:25 +0700

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-import "dotenv/config";
-import express from "express";
-import mongoose from "mongoose";
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require("./routes/userRoute")
 
 const app = express();
 
 app.use(express.json());
+app.use('/users', userRoutes)
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome");
