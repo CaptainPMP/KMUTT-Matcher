7c0edb99f933c4a585fe8bd7de26437caf93d67e
ศิรวิทย์ อาภาวัชรานันท์ <talnw1123>
2023-11-23 01:45:13 +0700

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// CSS
 import './App.css'
+import './components/LoginPage/Login.css'
+import './components/RegisterPage/Register.css'
+import './components/MainPage/MainPage.css'
+import './components/EditPage/EditPage.css'
+// Landing Page
+import Navbar from './components/LandingPage/Navbar'
+import Hero from './components/LandingPage/Hero'
+import About from './components/LandingPage/About'
+import InfoOne from './components/LandingPage/InfoOne'
+import InfoTwo from './components/LandingPage/InfoTwo'
+import CTA from './components/LandingPage/CTA'
+import Testimonials from './components/LandingPage/Testimonials'
+import GetInTouch from './components/LandingPage/GetInTouch'
+import Footer from './components/LandingPage/Footer'
+//Login Page
+import Login from './components/LoginPage/Login'
+// Register Page
+import Register from './components/RegisterPage/Register'
+//MainPage
+import BottomMenu from './components/MainPage/BottomMenu'
+import WelcomeMenu from './components/MainPage/WelcomeMenu'
+import BoxBox from './components/MainPage/BoxBox'
+//EditPage
+import EditPage from './components/EditPage/EditPage'
+import UserEdit from './components/EditPage/UserEdit'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <EditPage/>
+      <UserEdit/>
+      <BottomMenu/>
     </>
   )
 }
